fd30cbcccbd28cf227158a34139a856c97c93f25
Shahid N. Shah <shahid@shah.org>
2020-07-25 10:59:38 -0400

    Update dependencies

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import {
   InflectableValue,
   snakeCaseValue,
-} from "https://denopkg.com/shah/text-inflect@v1.0.3/mod.ts";
+} from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
 
 export interface Context {
   readonly isContext: true;
